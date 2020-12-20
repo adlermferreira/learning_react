f64656c54b8302b29cd64b37d92f43f6ef9ee79a
Adler Melgaço Ferreira <adlermf07@gmail.com>
2020-12-20 18:26:29 -0300

    added event handlers to forms in EditableTimer

--- public/js/app.js
+++ public/js/app.js
@@ -67,6 +67,27 @@ class EditableTimer extends React.Component{
         editFormOpen: false,
     };
 
+    handleEditClick = () => {
+        this.openForm();
+    };
+
+    handleFormClose = () => {
+        this.closeForm();
+    };
+
+    handleSubmit = (timer) => {
+        this.props.onFormSubmit(timer);
+        this.closeForm();
+    };
+
+    closeForm = () => {
+        this.setState({ editFormOpen = false });
+    };
+
+    openForm = () => {
+        this.setState({ editFormOpen = true });
+    };
+
     render(){
         if (this.state.editFormOpen){
             return(
@@ -74,6 +95,8 @@ class EditableTimer extends React.Component{
                     id={this.props.id}
                     title={this.props.title}
                     project={this.props.project}
+                    onFormSubmit={this.handleSubmit}
+                    onFormClose={this.handleFormClose}
                 />
             );
         } else {
@@ -84,6 +107,7 @@ class EditableTimer extends React.Component{
                     project={this.props.project}
                     elapsed={this.props.elapsed}
                     runningSince={this.props.runningSince}
+                    onEditClick={this.handleEditClick}
                 />
             );
         }

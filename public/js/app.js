3b0013ce21bc9ab3d132f34cadee304edbdf480e
Adler Melgaço Ferreira <adlermf07@gmail.com>
2020-12-20 13:39:45 -0300

    submit and cancel event handlers added on TimerForm

--- public/js/app.js
+++ public/js/app.js
@@ -92,8 +92,16 @@ class TimerForm extends React.Component{
         this.setState({ project: e.target.value });
     };
 
+    handleSubmit = () => {
+        this.props.onFormSubmit({
+            id: this.props.id,
+            title: this.state.title,
+            project: this.state.project,
+        });
+    };
+
     render(){
-        const submitText = this.props.title ? 'Update' : 'Create';
+        const submitText = this.props.id ? 'Update' : 'Create';
         return (
             <div className='ui centered card'>
                 <div className='content'>
@@ -115,10 +123,16 @@ class TimerForm extends React.Component{
                             />
                         </div>
                         <div className='ui two bottom attached buttons'>
-                            <button className='ui basic blue button'>
+                            <button 
+                                className='ui basic blue button'
+                                onClick={this.handleSubmit}
+                            >
                                 {submitText}
                             </button>
-                            <button className='ui basic red button'>
+                            <button 
+                                className='ui basic red button'
+                                onClick={this.props.onFormClose}
+                            >
                                 Cancel
                             </button>
                         </div>
